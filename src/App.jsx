d0019660f7a2e46f9a0a777bfb412bd6a8a3789b
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2025-06-02 14:06:40 +0530

    spreading props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import reactLogo from "./assets/react.svg";
 import User from "./User";
+import userData from "./user.json";
 
 const greets = ["Hello", "Hi", "Hey"];
-
-const users = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
-];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -21,15 +16,11 @@ function App() {
       <img src={reactLogo} alt="react logo" />
       <h1>{greet}, from React</h1>
       <ul>
-        <li>
-          <User id={users[0].id} name={users[0].name} />
-        </li>
-        <li>
-          <User id={users[1].id} name={users[1].name} />
-        </li>
-        <li>
-          <User id={users[2].id} name={users[2].name} />
-        </li>
+        {userData.map((user) => (
+          <li key={user.id}>
+            <User {...user} />
+          </li>
+        ))}
       </ul>
     </>
   );
