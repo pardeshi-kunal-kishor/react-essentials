87d9e69323af2284998f912636a42b162479dc88
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2025-06-02 14:01:56 +0530

    passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import reactLogo from "./assets/react.svg";
+import User from "./User";
 
 const greets = ["Hello", "Hi", "Hey"];
+
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -8,10 +15,22 @@ function genRandomInt(max) {
 
 function App() {
   const greet = greets[genRandomInt(2)];
+
   return (
     <>
       <img src={reactLogo} alt="react logo" />
       <h1>{greet}, from React</h1>
+      <ul>
+        <li>
+          <User id={users[0].id} name={users[0].name} />
+        </li>
+        <li>
+          <User id={users[1].id} name={users[1].name} />
+        </li>
+        <li>
+          <User id={users[2].id} name={users[2].name} />
+        </li>
+      </ul>
     </>
   );
 }
---
+++ src/User.jsx
@@ -0,0 +1,10 @@
+function User(props) {
+  return (
+    <>
+      <h1>{props.id}</h1>
+      <p>{props.name}</p>
+    </>
+  );
+}
+
+export default User;
